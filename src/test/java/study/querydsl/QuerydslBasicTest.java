e96d5fc16ff64ed45036e262f3cf11f832f98989
cousim46 <wjdghldns24@naver.com>
2022-07-07 23:47:29 +0900

    query를 이용한 page처리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,42 +1,33 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
-import com.querydsl.core.Tuple;
-import com.querydsl.core.types.dsl.CaseBuilder;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import javax.transaction.Transactional;
+
 import java.util.List;
 
-import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static study.querydsl.entity.QMember.*;
 import static study.querydsl.entity.QMember.member;
-import static study.querydsl.entity.QTeam.team;
 
 @SpringBootTest
 @Transactional
 public class QuerydslBasicTest {
     @PersistenceContext
     EntityManager em;
-    JPAQueryFactory jpaQueryFactory;
+    JPAQueryFactory queryFactory;
 
 
     @BeforeEach
     public void before() {
-        jpaQueryFactory = new JPAQueryFactory(em);
+        queryFactory = new JPAQueryFactory(em);
 
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
@@ -63,7 +54,7 @@ public class QuerydslBasicTest {
 
     @Test
     void startQuerydsl() {
-        Member findMember = jpaQueryFactory
+        Member findMember = queryFactory
                 .select(member)
                 .from(member)
                 .where(member.username.eq("member1"))
@@ -73,7 +64,7 @@ public class QuerydslBasicTest {
 
     @Test
     void search() {
-        Member findMember = jpaQueryFactory
+        Member findMember = queryFactory
                 .selectFrom(member)
                 .where(member.username.eq("member1").and(member.age.eq(10)))
                 .fetchOne();
@@ -83,7 +74,7 @@ public class QuerydslBasicTest {
 
     @Test
     void searchAndParam() {
-        Member findMember = jpaQueryFactory
+        Member findMember = queryFactory
                 .selectFrom(member)
                 .where(
                         member.username.eq("member1"),
@@ -96,26 +87,15 @@ public class QuerydslBasicTest {
 
     @Test
     void resultFetch() {
-        QueryResults<Member> results = jpaQueryFactory
-                .selectFrom(member)
-                .fetchResults();
-
-        // results.getTotal();
-        //List<Member> results1 = results.getResults();
-     /*   List<Member> fetch = jpaQueryFactory
+      /*  List<Member> fetch = queryFactory
                 .selectFrom(member)
                 .fetch();
+        Member fetchOne = queryFactory.selectFrom(member).fetchOne();
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();*/
+        QueryResults<Member> fetchResults = queryFactory.selectFrom(member).fetchResults();
 
-        Member fetchOne = jpaQueryFactory.selectFrom(member)
-                .fetchOne();
-
-        Member fetchFirst = jpaQueryFactory.selectFrom(member).fetchFirst();
-*/
-
-      /*  long total = jpaQueryFactory.selectFrom(member)
-                .fetchCount();
-        System.out.println("total = " + total);
-*/
 
     }
 
@@ -123,31 +103,34 @@ public class QuerydslBasicTest {
      * 회원 정렬 순서
      * 1. 회원 나이 내림차순(desc)
      * 2. 회원 이름 올림차순(asc)
-     * 단, 2에서 회원 이름이 없으면 마지막에 출력
-     */
+     * 단 2에서 회원 이름이 없으면 마지막에 출력
+     * */
     @Test
     void sort() {
         em.persist(new Member(null, 100));
         em.persist(new Member("member5", 100));
         em.persist(new Member("member6", 100));
-
-        List<Member> result = jpaQueryFactory
-                .selectFrom(member)
+        em.persist(new Member("member7", 100));
+        List<Member> result = queryFactory.selectFrom(member)
                 .where(member.age.eq(100))
                 .orderBy(member.age.desc(), member.username.asc().nullsLast())
                 .fetch();
 
         Member member5 = result.get(0);
         Member member6 = result.get(1);
-        Member memberNull = result.get(2);
+        Member member7 = result.get(2);
+        Member memberNull = result.get(3);
+
         assertThat(member5.getUsername()).isEqualTo("member5");
         assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(member7.getUsername()).isEqualTo("member7");
         assertThat(memberNull.getUsername()).isNull();
+
     }
 
     @Test
-    void paging1() {
-        List<Member> result = jpaQueryFactory
+    public void paging1() {
+        List<Member> result = queryFactory
                 .selectFrom(member)
                 .orderBy(member.username.desc())
                 .offset(1)
@@ -156,292 +139,20 @@ public class QuerydslBasicTest {
 
         assertThat(result.size()).isEqualTo(2);
     }
+     @Test
+    public void paging2() {
+         QueryResults<Member> memberQueryResults = queryFactory
+                 .selectFrom(member)
+                 .orderBy(member.username.desc())
+                 .offset(1)
+                 .limit(2)
+                 .fetchResults();
 
-    @Test
-    void paging2() {
-        QueryResults<Member> result = jpaQueryFactory
-                .selectFrom(member)
-                .orderBy(member.username.desc())
-                .offset(1)
-                .limit(2)
-                .fetchResults();
-        assertThat(result.getTotal()).isEqualTo(4);
-        assertThat(result.getLimit()).isEqualTo(2);
-        assertThat(result.getOffset()).isEqualTo(1);
-        assertThat(result.getResults().size()).isEqualTo(2);
-    }
+        assertThat(memberQueryResults.getTotal()).isEqualTo(4);
+        assertThat(memberQueryResults.getOffset()).isEqualTo(1);
+        assertThat(memberQueryResults.getLimit()).isEqualTo(2);
+        assertThat(memberQueryResults.getResults().size()).isEqualTo(2);
 
-    @Test
-    void aggregation() {
-        List<Tuple> result = jpaQueryFactory
-                .select(
-                        member.count(),
-                        member.age.sum(),
-                        member.age.avg(),
-                        member.age.max(),
-                        member.age.min()
-                ).from(member)
-                .fetch();
-
-        Tuple tuple = result.get(0);
-        assertThat(tuple.get(member.count())).isEqualTo(4);
-        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
-        assertThat(tuple.get(member.age.max())).isEqualTo(40);
-        assertThat(tuple.get(member.age.min())).isEqualTo(10);
-        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
-    }
-
-    /**
-     * 팀의 이름과 각 팀의 평균 연령을 구해라
-     */
-    @Test
-    void group() {
-        List<Tuple> result = jpaQueryFactory
-                .select(team.name, member.age.avg())
-                .from(member)
-                .join(member.team, team)
-                .groupBy(team.name)
-                .fetch();
-
-        for (Tuple tuple : result) {
-            System.out.println("tuple = " + tuple);
-        }
-
-        Tuple teamA = result.get(0);
-        Tuple teamB = result.get(1);
-
-        assertThat(teamA.get(team.name)).isEqualTo("teamA");
-        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
-
-        assertThat(teamB.get(team.name)).isEqualTo("teamB");
-        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
-
-    }
-
-    /***
-     * 팀 A에 소속된 모든회원
-     */
-    @Test
-    void join() {
-        List<Member> result = jpaQueryFactory
-                .selectFrom(member)
-                .join(member.team, team)
-                .where(team.name.eq("teamA"))
-                .fetch();
-        assertThat(result)
-                .extracting("username")
-                .containsExactly("member1", "member2");
-    }
-
-    /***
-     * 세타 조인
-     * 회원의 이름이 팀 이름과 같은 회원 조회
-     */
-    @Test
-    void theta_join() {
-        em.persist(new Member("teamA"));
-        em.persist(new Member("teamB"));
-        em.persist(new Member("teamC"));
-
-        List<Member> result = jpaQueryFactory
-                .select(member)
-                .from(member, team)
-                .where(member.username.eq(team.name)).fetch();
-
-        assertThat(result)
-                .extracting("username")
-                .containsExactly("teamA", "teamB");
-    }
-    /**
-     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
-     * jpql : select m, t from Member m left join m.team t on t.name = 'teamA'
-     */
-    @Test
-    void join_on_filtering() {
-        List<Tuple> result = jpaQueryFactory
-                .select(member, team)
-                .from(member)
-                .leftJoin(member.team, team).on(team.name.eq("teamA")).fetch();
-        for (Tuple tuple : result) {
-            System.out.println("tuple = " + tuple);
-        }
-    }
-
-    /***
-     * 연관관계가 없는 엔티티 외부조인
-     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
-     */
-    @Test
-    void join_on_no_relation() {
-        em.persist(new Member("teamA"));
-        em.persist(new Member("teamB"));
-        em.persist(new Member("teamC"));
-
-        List<Tuple> result = jpaQueryFactory
-                .select(member,team)
-                .from(member)
-                .leftJoin(team).on(member.username.eq(team.name)).fetch();
-
-        for (Tuple tuple : result) {
-            System.out.println("tuple = " + tuple);
-        }
-
-
-    }
-    @PersistenceUnit //
-    EntityManagerFactory emf;
-    @Test
-    void fetchJoinNo()  {
-        em.flush();
-        em.clear();
-        Member member = jpaQueryFactory
-                .selectFrom(QMember.member)
-                .where(QMember.member.username.eq("member1"))
-                .fetchOne();
-        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(member.getTeam());
-        assertThat(loaded).as("패치 조인 미적용").isFalse();
-    }
-    @Test
-    void fetchJoinUse()  {
-        em.flush();
-        em.clear();
-        Member member = jpaQueryFactory
-                .selectFrom(QMember.member)
-                .join(QMember.member.team, team).fetchJoin()
-                .where(QMember.member.username.eq("member1"))
-                .fetchOne();
-        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(member.getTeam());
-        assertThat(loaded).as("패치 조인 적용").isTrue();
-    }
-
-    /**
-     * 나이가 가장 많은 회원 조회
-     *
-     */
-
-    @Test
-    void subQuery() {
-
-        QMember memberSub = new QMember("memberSub");
-        List<Member> result = jpaQueryFactory
-                .selectFrom(member)
-                .where(member.age.eq(
-                        select(memberSub.age.max())
-                                .from(memberSub)
-                )).fetch();
-
-        assertThat(result)
-                .extracting("age")
-                .containsExactly(40);
-    }
-
-    /**
-     * 나이가 평균 이상인 회원
-     */
-    @Test
-    void subQueryGoe() {
-
-        QMember memberSub = new QMember("memberSub");
-        List<Member> result = jpaQueryFactory
-                .selectFrom(member)
-                .where(member.age.goe(
-                        select(memberSub.age.avg())
-                                .from(memberSub)
-                )).fetch();
-
-        assertThat(result)
-                .extracting("age")
-                .containsExactly(30,40);
-    }
-    /**
-     * 나이가 평균 이상인 회원
-     */
-    @Test
-    void subQueryIn() {
-
-        QMember memberSub = new QMember("memberSub");
-        List<Member> result = jpaQueryFactory
-                .selectFrom(member)
-                .where(member.age.in(
-                        select(memberSub.age)
-                                .from(memberSub)
-                                .where(memberSub.age.gt(10))
-                )).fetch();
-
-
-        assertThat(result)
-                .extracting("age")
-                .containsExactly(20,30,40);
-    }
-
-/**
-     * 나이가 평균 이상인 회원
-     */
-    @Test
-    void selectSubQuery() {
-        QMember memberSub = new QMember("memberSub");
-        List<Tuple> result = jpaQueryFactory
-                .select(member.username,
-                        select(memberSub.age.avg())
-                                .from(memberSub))
-                .from(member)
-                .fetch();
-
-        for (Tuple tuple : result) {
-            System.out.println("tuple = " + tuple);
-        }
-
-    }
-
-    @Test
-    void basicCase() {
-        List<String> result = jpaQueryFactory
-                .select(member.age
-                        .when(10).then("열살")
-                        .when(20).then("스무살")
-                        .otherwise("기타")).from(member).fetch();
-
-
-        for (String s : result) {
-            System.out.println("s = " + s);
-        }
-    }
-
-    @Test
-    void complexCase() {
-        List<String> result = jpaQueryFactory
-                .select(new CaseBuilder()
-                        .when(member.age.between(0, 20)).then("0~20살")
-                        .when(member.age.between(21, 30)).then("21~30살").otherwise("기타"))
-                .from(member)
-                .fetch();
-        for (String s : result) {
-            System.out.println("s = " + s);
-        }
-    }
-
-    @Test
-    void constant() {
-        List<Tuple> a = jpaQueryFactory
-                .select(member.username, Expressions.constant("A"))
-                .from(member)
-                .fetch();
-
-        for (Tuple tuple : a) {
-            System.out.println("tuple = " + tuple);
-        }
-    }
-
-    @Test
-    public void concat() {
-        List<String> result = jpaQueryFactory
-                .select(member.username.concat("_").concat(member.age.stringValue()))
-                .from(member)
-                .where(member.username.eq("member1"))
-                .fetch();
-        for (String s : result) {
-            System.out.println("s = " + s);
-        }
     }
 
 }
